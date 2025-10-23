16d50f69b69d3dc296775f5bb63ed4898f53165c
Carlo Cobey <carlocobey0204@gmail.com>
2025-10-23 11:47:56 +0800

    Add API and logging to API, encryption and decryption endpoints, and update validation of data

---
+++ api/app.py
@@ -0,0 +1,52 @@
+import logging
+from flask import Flask, jsonify, request
+from encryption.aes_encryption import encrypt_message, decrypt_message
+
+# Create flask app
+app = Flask(__name__)
+
+# Configure logging 
+
+logging.basicConfig(
+    filename='api.log',         # single log file for the project
+    level=logging.INFO,         #capture INFO, WARNING, ERROR, etc
+    format='%(asctime)s - $(levelname)s - $(message)s'
+)
+
+
+@app.route("/api/encrypt", methods=["POST"])
+
+
+def encrypt():
+
+    data = request.json # get JSON payload from client
+
+    if not data or "message" not in data:
+        logging.warning("[API] Encrypt request missing 'message'")
+        return jsonify({"error": "No message provided"}), 400
+
+    plaintext = data.get("message")
+    ciphertext = encrypt_message(plaintext)
+    logging.info(f"[API][ENCRYPTION] Encrypted message successfully")
+    return jsonify({"encrypted": ciphertext})
+
+
+@app.route("/api/decrypt", methods=["POST"])
+
+
+def decrypt():
+
+    data = request.json
+
+    if not data or "ciphertext" not in data:
+        logging.warning("[API] Decrypt request missing 'ciphertext")
+        return jsonify({"error": "No ciphertext provided"}), 400
+    
+    ciphertext = data.get("ciphertext")
+    message = decrypt_message(ciphertext)
+    logging.info(f"[API][DECRYPTION] Decrypted message successfully")
+    return jsonify({"decrypted": message})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
